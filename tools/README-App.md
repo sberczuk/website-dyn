22f47cbb3df2c9d6320ecfe067900559730f8681
Steve Berczuk <sberczuk@fitbit.com>
2017-10-24 18:21:52 -0400

    Intital template plus xml to JSON tooks + Readme from early experiments. Next commit will have the basic template for the books page and the index page.

---
+++ tools/README-App.md
@@ -0,0 +1,21 @@
+Steps to build the app
+
+*  ember new bookstore
+*  ember serve : verify that we work
+*  cp ../../websitev2/ember-bookstore/public/books.json public/ : copy json file
+*  ember serve
+*  ember g model book
+*  ember generate route shop
+
+The thing I was missing was that I was not following JSON API. I needed a data and Attributes section.
+
+My next project is to write a script (Ruby? Groovy?) to convert my XML to a JSON API compliant form.
+
+the code in xmlSrc/xmlToJson.groovy does the conversion. It's not quite there, as it has attributes as lists, not maps.
+
+It may be better to use JSONBuilder syntax rather than mapping Object to JSON
+
+
+
+## Note:
+Ember new creates a git repo. Who knew?
---
+++ tools/xmlToJson.groovy
@@ -0,0 +1,61 @@
+def xmlFile = new File("books.xml")
+println xmlFile.size()
+
+
+def parser = new XmlSlurper()
+def books=parser.parse(xmlFile)
+// get each node name and value
+int i=0
+
+
+
+
+// def jsonObject = [ data: books.book.collect {b->
+//   [type: "book", id: i++,
+//   attributes:
+//   b.children().collect{ it->
+//     if(it.children().size() == 0 ){
+//     ["${it.name()}": it.text()] // add closure to handle nested
+//   } else{
+//     [(it.name()) : it.children().collect{ a->
+//        [(a.name()): a.text()]
+//       }]
+//   }
+//   }
+//
+//   ]}]
+
+// only saves the last item!
+def jsonObject = [ data: books.book.collect {b->
+  [type: "book", id: i++,
+  attributes:
+  b.children().collectEntries{ it->
+    if(it.children().size() == 0 ){
+    ["${it.name()}": it.text()] // add closure to handle nested
+  } else{
+    [(it.name()) : it.children().collectEntries{ a->
+       [(a.name()): a.text()]
+      }]
+  }
+  }
+
+  ]}]
+println jsonObject
+
+
+
+
+
+//println books.book[0].each{v-> v.name v.value}
+def json = new groovy.json.JsonBuilder(jsonObject)
+
+println ">> Printing"
+println json.toPrettyString()
+def outFile = new File("books.json")
+println ">> Printing"
+println ">> Entries " + i
+
+outFile.write json.toPrettyString()
+// outFile.withWriter('UTF-8') { writer ->
+//     writer.write(json.toPrettyString())
+// }
