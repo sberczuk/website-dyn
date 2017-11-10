d63891d6e54ce84b9f28137ba888cbef63bcf94c
Steve Berczuk <sberczuk@fitbit.com>
2017-11-10 12:27:56 -0500

    add more data to model

--- app/models/book.js
+++ app/models/book.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   isbn: DS.attr('string'),
-    comments: DS.attr('string'),
+  comments: DS.attr('string'),
+  categories: DS.attr(),
+  seeAlso: DS.hasMany('book'  {inverse:null}),
 });
